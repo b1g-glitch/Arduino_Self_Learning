bd6c5bd98e4ffbb5eddc77f06a9d3dd97b151e72
Tope Ajayi <Youngmetre@gmail.com>
2025-10-07 23:24:05 +0100

    Add files via upload

---
+++ Love-o-Meter_Thermometer.ino
@@ -0,0 +1,46 @@
+const int sensorPin = A0;
+const float baseTemp = 20.0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  for(int pinNumber = 2; pinNumber < 5; pinNumber++){
+    pinMode(pinNumber, OUTPUT);
+    digitalWrite(pinNumber, LOW);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int sensorVal = analogRead(sensorPin);
+  Serial.print("Sensor Value: ");
+  Serial.print(sensorVal);
+  float voltage = (sensorVal/1024.0) * 5.0;
+  Serial.print(", Volts: ");
+  Serial.print(voltage);
+  Serial.print(", Degrees C: ");
+  // convert the voltage to temperature in degrees
+  float temperature = (voltage - 0.5) * 100;
+  Serial.println(temperature);
+
+  if(temperature < baseTemp + 2){
+    digitalWrite(2, LOW);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  }else if(temperature >= baseTemp + 2 && temperature < baseTemp + 4){
+    // for LOW temperature
+    digitalWrite(2, HIGH);
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+  }else if(temperature >= baseTemp + 4 && temperature < baseTemp + 6){
+    // for MEDIUM temperature
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+  }else if(temperature >= baseTemp + 6){
+    // for HIGH temperature
+    digitalWrite(2, HIGH);
+    digitalWrite(3, HIGH);
+    digitalWrite(4, HIGH);
+  }
+  delay(1);
+}
